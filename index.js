4c5be93bb1eafa500803f461c5586f94cbc039f6
eph-eye-en <>
2022-02-27 02:13:31 +0000

    Add basic cafe login

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const PORT = 8080;
+const SECRET = "super-secret-string";
 
 const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
 
 const app = express();
+
+const LocalStrategy = require("passport-local").Strategy;
+
+app.use(session({
+	secret: SECRET,
+	resave: false,
+	saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+	usernameField: "email",
+	passwordField: "password",
+	passReqToCallback: true
+}, authUser));
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/login", passport.authenticate("local", {
+	successRedirect: "/cafe.html",
+	failureRedirect: "/cafeLogin.html"
+}));
 
 app.get("/api/available", (req, res) => {
 	const data = [];
@@ -26,3 +55,11 @@ app.use(express.static("./static"));
 app.listen(PORT);
 
 console.log(`Running on port ${PORT}`);
+
+function authUser(req, user, password, done) {
+	if(password != "Password")
+		return done(null, false);
+
+	const authedUser = { id: 123, name: user };
+	return done(null, authedUser);
+}
